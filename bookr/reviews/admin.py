e93bd5298609b288c9c0f041166467532df16170
Matt Skelley <mattskelley.au@gmail.com>
2023-09-29 20:25:54 +0800

    The search bar

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -2,9 +2,20 @@ from django.contrib import admin
 from reviews.models import (Publisher, Contributor, Book, BookContributor, Review)
 
 
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'isbn', 'publisher__name')
+    date_hierarchy = 'publication_date'
+    list_display = ('title', 'isbn13', 'has_isbn')
+    list_filter = ('publisher', 'publication_date')
+
+    @admin.display(boolean=True, description='Has ISBN')
+    def has_isbn(self, obj):
+        return bool(obj.isbn)
+
+
 # Register your models here.
 admin.site.register(Publisher)
 admin.site.register(Contributor)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
 admin.site.register(Review)
