ecbf186ea65579378825f00c83e1781312ed7478
Matt Skelley <mattskelley.au@gmail.com>
2023-12-04 18:52:38 +0800

    Activity 7.01 - styling and integrating the publisher form

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -83,6 +83,10 @@ def publisher_edit(request, pk=None):
     else:
         form = PublisherForm(instance=publisher)
 
-    return render(request, "reviews/form-example.html", {"method": request.method, "form": form})
+    return render(request, "reviews/instance-form.html", {
+        "form": form, 
+        "publisher": publisher,
+        "model_type": "Publisher"
+    })
 
     
