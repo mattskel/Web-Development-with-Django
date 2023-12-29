f70dee1241cbe1c74697ff9fd8704c1ceefb9462
Matt Skelley <mattskelley.au@gmail.com>
2023-12-29 20:02:48 +0800

    Exercise 8.06 - FileField and ImageField on models

--- media_project/media_example/views.py
+++ media_project/media_example/views.py
@@ -1,21 +1,21 @@
 import os
 from django.shortcuts import render
-from django.conf import settings
 
 from .forms import UploadForm
-from PIL import Image
+from .models import ExampleModel
 
 # Create your views here.
 def media_example(request):
+  instance = None
   if request.method == 'POST':
     form = UploadForm(request.POST, request.FILES)
     if form.is_valid():
-      save_path = os.path.join(settings.MEDIA_ROOT, form.cleaned_data["file_upload"].name)
-      image = Image.open(form.cleaned_data['file_upload'])
-      image.thumbnail((50, 50))
-      image.save(save_path)
+      instance = ExampleModel()
+      instance.image_field = form.cleaned_data['image_upload']
+      instance.file_field = form.cleaned_data['file_upload']
+      instance.save()
 
   else:
     form = UploadForm()
   
-  return render(request, 'media-example.html', {'form': form})
+  return render(request, 'media-example.html', {'form': form, 'instance': instance})
