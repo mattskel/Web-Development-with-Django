9f5ad5822a40ea5e834a75d5abdd23af65e00ade
Matt Skelley <mattskelley.au@gmail.com>
2023-12-29 14:56:04 +0800

    Exercise 8.03 - file upload and download

--- media_project/media_example/views.py
+++ media_project/media_example/views.py
@@ -1,5 +1,15 @@
+import os
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def media_example(request):
+  if request.method == 'POST':
+    # save_path = settings.MEDIA_ROOT / request.FILES['file_upload'].name
+    save_path = os.path.join(settings.MEDIA_ROOT, request.FILES["file_upload"].name)
+
+    with open(save_path, 'wb') as output_file:
+      for chunk in request.FILES['file_upload'].chunks():
+        output_file.write(chunk)
+
   return render(request, 'media-example.html')
