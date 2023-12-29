56226e523fed5f65d425b453d315f060790c5013
Matt Skelley <mattskelley.au@gmail.com>
2023-12-29 16:32:37 +0800

    Exercise 8.05 - image uploads using Django forms

--- media_project/media_example/views.py
+++ media_project/media_example/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.conf import settings
 
 from .forms import UploadForm
+from PIL import Image
 
 # Create your views here.
 def media_example(request):
@@ -10,10 +11,9 @@ def media_example(request):
     form = UploadForm(request.POST, request.FILES)
     if form.is_valid():
       save_path = os.path.join(settings.MEDIA_ROOT, form.cleaned_data["file_upload"].name)
-
-      with open(save_path, 'wb') as output_file:
-        for chunk in form.cleaned_data['file_upload'].chunks():
-          output_file.write(chunk)
+      image = Image.open(form.cleaned_data['file_upload'])
+      image.thumbnail((50, 50))
+      image.save(save_path)
 
   else:
     form = UploadForm()
