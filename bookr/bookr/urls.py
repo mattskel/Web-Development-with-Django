fd9c2275e02325288fab67db672df74be85977db
Matt Skelley <mattskelley.au@gmail.com>
2024-01-01 16:31:06 +0800

    Exercise 10.02 - overriding the default admin site

--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -21,12 +21,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from bookr.views import profile
-from bookr_admin.admin import admin_site
 
 urlpatterns = [
     path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='accounts')),
     path('accounts/profile/', profile, name='profile'),
-    path('admin/', admin_site.urls),
+    path('admin/', admin.site.urls),
     path('', include('reviews.urls'))
 ]
 
