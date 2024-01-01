d1530b96b8bf4b70fdeaccdd475fe6989cecf00e
Matt Skelley <mattskelley.au@gmail.com>
2024-01-01 16:03:39 +0800

    Exercise 10.01 - creating a custom admin site for Bookr

--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -21,11 +21,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from bookr.views import profile
+from bookr_admin.admin import admin_site
 
 urlpatterns = [
     path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='accounts')),
     path('accounts/profile/', profile, name='profile'),
-    path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
     path('', include('reviews.urls'))
 ]
 
