01857750ee0e753acf954d1e22d6b1b9fb8a4e77
Matt Skelley <mattskelley.au@gmail.com>
2024-01-02 19:35:44 +0800

    Exercise 11.03 - Building a custom inclusion tag

--- bookr/filter_demo/views.py
+++ bookr/filter_demo/views.py
@@ -6,4 +6,8 @@ def index(request):
     return render(request, "index.html", {'names': names})
 
 def greeting_view(request):
-    return render(request, 'simple_tag_template.html', {'username': 'jdoe'})
+    books = {
+        "The night rider": "Ben Author",
+        "The Justice": "Don Abeman"
+    }
+    return render(request, 'simple_tag_template.html', {'username': 'jdoe', 'books': books})
