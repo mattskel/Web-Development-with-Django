cc55cb4c43524ccfd5b07ab50b29f4bcb0c1c767
Matt Skelley <mattskelley.au@gmail.com>
2024-01-03 20:45:46 +0800

    Exercise 12.03 - creating class-based API views and model serializers

--- bookr/reviews/api_views.py
+++ bookr/reviews/api_views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
 
 from .models import Book
 from .serializers import BookSerializer
@@ -14,3 +15,7 @@ def all_books(request):
     books = Book.objects.all()
     book_serializer = BookSerializer(books, many=True)
     return Response(book_serializer.data)
+
+class AllBooks(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
