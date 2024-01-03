b1f5c24e6f08a8301fa6e9cec07b94a5085858b5
Matt Skelley <mattskelley.au@gmail.com>
2024-01-03 22:31:40 +0800

    Exercise 12.04 - using ViewSets and routers

--- bookr/reviews/api_views.py
+++ bookr/reviews/api_views.py
@@ -1,9 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, viewsets
+from rest_framework.pagination import LimitOffsetPagination
 
-from .models import Book, Contributor
-from .serializers import BookSerializer, ContributorSerializer
+from .models import Book, Contributor, Review
+from .serializers import BookSerializer, ContributorSerializer, ReviewSerializer
 
 @api_view()
 def first_api_view(request):
@@ -23,3 +24,13 @@ class AllBooks(generics.ListAPIView):
 class ContributorView(generics.ListAPIView):
     queryset = Contributor.objects.all()
     serializer_class = ContributorSerializer
+
+class BookViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.order_by('-date_created')
+    serializer_class = ReviewSerializer
+    pagination_class = LimitOffsetPagination
+    authentication_classes = []
