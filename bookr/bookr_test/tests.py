5e8a1dadbafb4248b1db262fc7ffe80f15e1851d
Matt Skelley <mattskelley.au@gmail.com>
2024-01-04 22:52:41 +0800

    Exercise 14.04 - writing test cases to validate authenticated users

--- bookr/bookr_test/tests.py
+++ bookr/bookr_test/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from django.contrib.auth.models import User
 from .models import Publisher
 
 # Create your tests here.
@@ -24,4 +25,20 @@ class TestGreetingView(TestCase):
 
     def test_greeting_view(self):
         response = self.client.get('/test/greeting')
+        self.assertEquals(response.status_code, 200)
+
+class TestLoggedInGreetingView(TestCase):
+    """Test the greeting view for the authenticated user."""
+    def setUp(self):
+        test_user = User.objects.create_user(username='testuser', password='test@#628password')
+        test_user.save()
+        self.client = Client()
+
+    def test_user_greeting_not_authenticated(self):
+        response = self.client.get('/test/greet_user')
+        self.assertEquals(response.status_code, 302)
+
+    def test_user_authenticated(self):
+        login = self.client.login(username='testuser', password='test@#628password')
+        response = self.client.get('/test/greet_user')
         self.assertEquals(response.status_code, 200)
