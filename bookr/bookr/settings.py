7317f53a29a594093bad714e08da91a870ee557b
Matt Skelley <mattskelley.au@gmail.com>
2024-01-06 14:58:10 +0800

    Exercise 15.02 - dj-database-url and setup

--- bookr/bookr/settings.py
+++ bookr/bookr/settings.py
@@ -81,12 +81,16 @@ class Dev(Configuration):
     # Database
     # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
+    DATABASES = values.DatabaseURLValue(
+        f'sqlite:///{BASE_DIR}/db.sqlite3',
+        environ_prefix='DJANGO'
+    )
 
 
     # Password validation
